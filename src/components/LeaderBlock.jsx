51dab92b49cb698575bc77c5934f2f7b3b7126ae
Student <example@example.com>
2020-02-13 16:31:06 -0800

    add names, titles and fix layout of leaderblock

--- src/components/LeaderBlock.jsx
+++ src/components/LeaderBlock.jsx
@@ -1,51 +1,66 @@
 import React from 'react';
 
 const imageFiles = [
-  'Adelle',
-  'Adolfo',
-  'Alberta',
-  'Amelia',
-  'Amy',
-  'Andre',
-  'Andrew',
-  'Anette',
-  'Angela',
-  'Ann',
-  'Arthur'
+  ['Adelle', 'Fowler', 'CEO'],
+  ['Adolfo', 'Germaine', 'Finance'],
+  ['Alberta', 'Gray', 'Legal'],
+  ['Amelia', 'Holloway', 'International Sales'],
+  ['Amy', 'Moran', 'Engineering'],
+  ['Andre', 'Morgan', 'People'],
+  ['Andrew', 'Pittman', 'Product'],
+  ['Anette', 'Romero', 'Customer Success'],
+  ['Angela', 'Ryan', 'American Sales'],
+  ['Ann', 'Stevenson', 'Technology'],
+  ['Arthur', 'Warren', 'Marketing']
 ];
 
 
 function LeaderBlock () {
+
   const pic = {
     width: '200px',
     height: '200px',
     borderRadius: '50%',
-    margin: '5px 3%'
-  }
-  const makeImages = (arr) => {
-    return arr.map((img, ind) =>
-      (<img alt={img} key={ind} src={`/staff/${img}.jpg`} style={pic}/>)
-    );
-  }
+    margin: '5px'
+  };
+  const cardStylez = {
+    marginLeft: '2%',
+    marginRight: '2%'
+  };
   const faces = {
-    display: 'flex-box',
+    display: 'flex',
     textAlign: 'center',
     alignItems: 'center',
-    flexWrap: 'wrap',
+    flexFlow: 'row wrap',
     maxWidth: '1440px',
     justifyContent: 'center',
     marginLeft: 'auto',
     marginRight: 'auto'
+  };
+
+  const makeCards = (arr) => {
+    return arr.map((img, ind) =>
+      (
+        <div key={ind} style={cardStylez}>
+          <img alt={`${img[0]}`} src={`/staff/${img[0]}.jpg`} style={pic}/>
+          <h5><strong>{`${img[0]} ${img[1]}`}</strong></h5>
+          <h5>{`${img[2]}`}</h5>
+        </div>
+    ));
   }
+
   return (
     <div style={faces}>
-      {makeImages(imageFiles)}
+      <style jsx>
+        {`
+          h5 {
+            margin-bottom: 15px;
+            margin-top: 5px;
+          }
+          `}
+      </style>
+      {makeCards(imageFiles)}
     </div>
-
-
-
-
-
   );
 };
 
